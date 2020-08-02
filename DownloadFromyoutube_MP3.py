668e9027da8f16362730f214860b0e2501751ec2
Omar Mohamed Ahmed <45721757+OmarMohammed88@users.noreply.github.com>
2020-08-02 10:17:25 +0200

    Update DownloadFromyoutube_MP3.py

--- DownloadFromyoutube_MP3.py
+++ DownloadFromyoutube_MP3.py
@@ -1,37 +1,7 @@
 import sys
-import os 
-from bs4 import BeautifulSoup
-#import requests
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options  
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import pandas as pd
-from pymongo import MongoClient
-from collections.abc import Iterable
-import warnings
 import time
-import requests
-import urllib.request as urllib2
-from bs4 import BeautifulSoup
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import StaleElementReferenceException
-from selenium.common.exceptions import ElementClickInterceptedException
-import selenium.common.exceptions
-from PIL import Image
-import requests
-from io import BytesIO
-import cv2 
 import re
-import random
-from langdetect import detect
-from selenium.webdriver.chrome.options import Options
 from fake_useragent import UserAgent
 ua = UserAgent()
 a = ua.random
@@ -40,13 +10,8 @@ user_agent = ua.random
 def download_song(url):
     url=re.sub("youtube","yout",url)
     driver.get(url)
-#    formatTomp3_button=driver.find_element_by_class_name("btn.btn-primary.btn-block.btn-yout.btn-recorder").click()
-#    time.sleep(1)
-#    button_128kb_=driver.find_element_by_class_name("search-record").click()
-#    time.sleep(1)
-#    go_tovideo_page=driver.find_element_by_class_name("btn.btn-default.to-video-page").click()
     time.sleep(1)
-    download_button=driver.find_element_by_class_name("btn.btn-primary.btn-block.btn-yout.btn-recorder").click()
+    driver.find_element_by_class_name("btn.btn-primary.btn-block.btn-yout.btn-recorder").click()
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--incognito")
